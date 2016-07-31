e38a29e750440149e30bb1e41c82452c2266141c
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2016-07-31 14:29:40 +0200

    RoleIcons by nebula

--- custom.lua
+++ custom.lua
@@ -86,6 +86,40 @@ SlashCmdList["CustomStuffSlashCommands"] = CustomStuff_SlashCommandHandler
 --SlashCmdList["CustomStuffSlashCommands"] = CustomStuff_FetteListeParsen
 ]]
 
+
+--RoleIcons
+local function roleIconsInit()
+	local roleIcons = setmetatable({}, { __index = function(t,i)
+			local parent = _G["RaidGroupButton"..i]
+			local icon = CreateFrame("Frame", nil, parent)
+			icon:SetSize(14, 14)
+			icon:SetPoint("RIGHT", parent.subframes.level, "LEFT", 2, 0)
+			RaiseFrameLevel(icon)
+	
+			icon.texture = icon:CreateTexture(nil, "ARTWORK")
+			icon.texture:SetAllPoints()
+			icon.texture:SetTexture("Interface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES")
+			icon:Hide()
+	
+			t[i] = icon
+			return icon
+	end })
+	
+	hooksecurefunc("RaidGroupFrame_Update", function()
+			for i = 1, GetNumGroupMembers() do
+					local button = _G["RaidGroupButton"..i]
+					if button and button.subframes then --make sure the raid button is set up
+							local icon = roleIcons[i]
+							local role = UnitGroupRolesAssigned("raid"..i)
+							if role and role ~= "NONE" then
+									icon.texture:SetTexCoord(GetTexCoordsForRoleSmallCircle(role))
+									icon:Show()
+							else
+									icon:Hide()
+							end
+					end
+			end
+	end)
 end
 
 
@@ -130,7 +164,10 @@ local function OnEvent(self, event, ...)
 	    if (select(1,...)) == "CrowBar" then
 	        CrowBarButton:SetScale(0.7)
 	    end
-	    if (select(1,...)) == "_CustomStuff" then
+--[[		if (select(1,...)) == "Blizzard_RaidUI" then
+			roleIconsInit()
+		end
+]]	    if (select(1,...)) == "_CustomStuff" then
 	        DoCustomStuff()
 	    end
     end
