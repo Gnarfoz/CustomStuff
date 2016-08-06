390e8d816d2688edf2daddb2b48ae66cfaa0163b
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2016-08-06 23:29:39 +0200

    Kill debug

--- custom.lua
+++ custom.lua
@@ -67,7 +67,7 @@ do
 		end
 		
 		if gearsets[id] then
-			print("Spec: "..id..", switching to equipment set: "..gearsets[id])
+			--print("Switching to equipment set: "..gearsets[id])
 			EquipmentManager_EquipSet(gearsets[id])
 		end
 	end
@@ -75,7 +75,7 @@ do
 	local frame = CreateFrame("Frame")
 	frame:SetScript("OnEvent", function(self, event, unit)
 			if unit ~= "player" then return end
-			print("P_S_C fired")
+			--print("P_S_C fired")
 			C_Timer.After(1, equipSet)
 	end)
 	frame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
