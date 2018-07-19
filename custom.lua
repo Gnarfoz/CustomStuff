e0dd53255d4874a1eef4cf55c57bc970f441e2f0
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2018-07-19 23:44:27 +0200

    Stats!

--- custom.lua
+++ custom.lua
@@ -29,7 +29,27 @@ SetCVar("screenshotQuality", 10)
 --PlayerPowerBarAlt verschieben
 --/run PlayerPowerBarAlt:ClearAllPoints(); PlayerPowerBarAlt:SetPoint("BOTTOM", UIParent, -200, 350)
 
+-- add the movement speed to the character stat sheet --stolen from NevMod
+function PaperDollFrame_SetMovementSpeed(statFrame, unit)
+	statFrame.wasSwimming = nil
+	statFrame.unit = unit
+	MovementSpeed_OnUpdate(statFrame)
+
+	statFrame.onEnterFunc = MovementSpeed_OnEnter
+	statFrame:SetScript("OnUpdate", MovementSpeed_OnUpdate)
+	statFrame:Show()
+end
+
+CharacterStatsPane.statsFramePool.resetterFunc =
+	function(pool, frame)
+		frame:SetScript("OnUpdate", nil)
+		frame.onEnterFunc = nil
+		frame.UpdateTooltip = nil
+		FramePool_HideAndClearAnchors(pool, frame)
 	end
+table.insert(PAPERDOLL_STATCATEGORIES[1].stats, { stat = "MOVESPEED"})
+
+
 
 do --marker keybinds
 	for i=0,8 do
